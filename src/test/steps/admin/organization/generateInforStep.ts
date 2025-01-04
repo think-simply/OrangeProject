59a79642c9a4a956a62414b3a9e95475da4c50dd
HuyenNT0420 <huyenhuyenneu04@gmail.com>
2025-01-04 23:30:27 +0700

    commit to fix BeforeStep

--- src/test/steps/admin/organization/generateInforStep.ts
+++ src/test/steps/admin/organization/generateInforStep.ts
@@ -1,34 +1,31 @@
-import { Given, When, Then } from "@cucumber/cucumber";
+import { Given, When, Then, BeforeStep } from "@cucumber/cucumber";
 import OrganizationAdminPage from "../../../pages/admin/organization/generalInforPage";
 import { pageFixture } from "../../../../hooks/pageFixture";
-
+let adminMenuPage: OrganizationAdminPage;
+BeforeStep( async() => {
+  adminMenuPage = new OrganizationAdminPage(pageFixture.adminPage);
+})
 //Pre-condition
 Given("User accesses General information in Organization", async () => {
-  const adminMenuPage = new OrganizationAdminPage(pageFixture.adminPage);
   await adminMenuPage.accessGI();
 });
 
 //GI_01: Check UI of Organization General Information
 Then("User should observe that all elements are displayed as expected", async () => {
-    const adminMenuPage = new OrganizationAdminPage(pageFixture.adminPage);
     await adminMenuPage.checkUIGI();
   }
 );
 
 //GI_02: Edit Organization General Information
 When("User turns the Edit toggle on", async () => {
-  const adminMenuPage = new OrganizationAdminPage(pageFixture.adminPage);
   await adminMenuPage.turnOnEditMode();
 });
 When("User updates some data", async () => {
-  const adminMenuPage = new OrganizationAdminPage(pageFixture.adminPage);
   await adminMenuPage.updateData();
 });
 When("User save the updated the data", async () => {
-  const adminMenuPage = new OrganizationAdminPage(pageFixture.adminPage);
   await adminMenuPage.saveUpdates();
 });
 Then("The information should be updated successfully", async () => {
-  const adminMenuPage = new OrganizationAdminPage(pageFixture.adminPage);
   await adminMenuPage.saveSuccessfully();
 });
