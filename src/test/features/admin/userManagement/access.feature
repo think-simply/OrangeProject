18dd191e4370955984c98bee4d785952af8a8d03
think-simply <nguyentheutb94@gmail.com>
2025-01-20 17:24:48 +0700

    testcase authorization 2 roles

--- src/test/features/admin/userManagement/access.feature
+++ src/test/features/admin/userManagement/access.feature
@@ -1,6 +1,7 @@
 Feature: Access Control Testing
  @multiUserTest
   Scenario: Staff should not have access to admin menu
-    Given Staff access to sites
+    Given Admin can access to admin menu
+    When Staff access to sites
     When Staff tries to access the admin menu
     Then Staff should see access denied message
--- src/test/steps/admin/userManagement/accessSteps.ts
+++ src/test/steps/admin/userManagement/accessSteps.ts
@@ -3,18 +3,22 @@ import UserPage from "#test/pages/admin/userManagement/userPage";
 import { pageFixture } from "#hooks/pageFixture";
 let userPage: UserPage;
 
-
-Given('Staff access to sites', async function() {
+Given('Admin can access to admin menu', async function () {
+    userPage = new UserPage(pageFixture.adminPage);
+    await userPage.visit();
+    await userPage.verifyUserPageUI();
+});
+When('Staff access to sites', async function () {
     userPage = new UserPage(pageFixture.staffPage);
     await userPage.staffAccessAdminMenu();
 });
 
-When('Staff tries to access the admin menu', async function() {
+When('Staff tries to access the admin menu', async function () {
     userPage = new UserPage(pageFixture.staffPage);
     await userPage.staffAccessAdminMenu();
 });
 
-Then('Staff should see access denied message', async function() {
+Then('Staff should see access denied message', async function () {
     userPage = new UserPage(pageFixture.staffPage);
     await userPage.verifyStaffAccessAdminMenu();
 });
